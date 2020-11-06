ac2adeb5f7bef48c62fec728856c4f1e11490cc2
Adludio <adludio@reids-mbp.home>
2020-11-06 09:26:39 +0000

    Update Tries component to show tally.tries

--- src/components/Tries.js
+++ src/components/Tries.js
@@ -5,8 +5,8 @@ import { useSelector } from "react-redux";
 // Display the number of tries, i.e 'You've tried x times'
 
 function Tries() {
-  const tally = useSelector(state => state.tally);
-  return "tries";
+  const tally = useSelector((state) => state.tally);
+  return <div>{tally.tries} Attempts</div>;
 }
 
 export default Tries;
