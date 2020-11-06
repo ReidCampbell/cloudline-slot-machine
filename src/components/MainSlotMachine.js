cc03706a45f992fe2ca4c47d28b55c638845f114
Adludio <adludio@reids-mbp.home>
2020-11-06 09:28:33 +0000

    Create the main slot machine

--- src/components/MainSlotMachine.js
+++ src/components/MainSlotMachine.js
@@ -6,17 +6,18 @@ import Tries from "./Tries";
 import Wins from "./Wins";
 
 const Parent = styled.div`
-  height: 100%;
   width: 100%;
   background: #dcdcf3;
   display: flex;
   align-items: center;
   justify-content: space-around;
+  flex-wrap: wrap;
 `;
 
 const SubDiv = styled.div`
-  height: 80%;
+  height: 80vh;
   width: 40%;
+  min-width: 350px;
   margin: 20px;
   padding: 30px;
   display: flex;
@@ -24,6 +25,8 @@ const SubDiv = styled.div`
   align-items: center;
   flex-direction: column;
   background: #e09e9e;
+  max-height: 460px;
+  border-radius: 25px;
 `;
 
 const Header = styled.div`
@@ -39,17 +42,20 @@ const Header = styled.div`
 
 const Slots = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
+  width: 100%;
 `;
 
 const Slot = styled.div`
   height: 250px;
-  width: 180px;
+  width: 35%;
   border: 2px solid black;
+  background-color: ${(props) => props.color};
+  border-radius: ${(props) => props.borderRadius};
 `;
 
 const Spin = styled.button`
-  width: 300px;
+  width: 100%;
   height: 100px;
   display: flex;
   justify-content: center;
@@ -59,6 +65,8 @@ const Spin = styled.button`
   background: blue;
   color: white;
   user-select: none;
+  border-radius: 10px;
+  border: none;
   :hover {
     cursor: pointer;
   }
@@ -77,7 +85,7 @@ const MainSlotMachine = () => {
   const dispatch = useDispatch();
 
   // Getting our main tally data from redux state.
-  const tally = useSelector(state => state.tally);
+  const tally = useSelector((state) => state.tally);
 
   // A few random base colors. To worsen the odds of winning,
   // you can add more colors.
@@ -92,19 +100,41 @@ const MainSlotMachine = () => {
   // every time we press the Spin button. This function should:
 
   // 1. Add to our tally tries in the redux state. (i.e dispatch(addToTries()))
-
   // 2. Randomly select a color 3 times from our base colors, and
   // set them in our local state above, newColors.
 
   // 3. If all the colors are the same, we add to our tally wins.
-  function spin() {}
+
+  // Get a random color from our base colors
+  const getRandomColor = () =>
+    baseColors[Math.floor(Math.random() * baseColors.length)];
+
+  // Check if the user has won
+  const checkIfWin = (colors) => colors.every((color) => color === colors[0]);
+
+  function spin() {
+    dispatch(addToTries());
+    const randomColors = [getRandomColor(), getRandomColor(), getRandomColor()];
+
+    // If the user has won add a win
+    checkIfWin(randomColors) && dispatch(addToWins());
+
+    setColors(randomColors);
+  }
 
   // TASK
   // In this lifecycle function, of the tally wins reaches 5,
   // have a window.confirm message come up telling the user to 'Stop Gambling!'.
   // on 5 wins the spin button should also become disabled.
   // On selecting 'ok', the tally wins and tries are reset.
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (tally.wins === 5) {
+      const confirmation = window.confirm("Stop Gambling!");
+
+      // Reset tally on player confirmation
+      confirmation && dispatch(resetTally());
+    }
+  }, [dispatch, tally.wins]);
 
   // TASK
   // Within the Slots div, create 3 slots. (Create a styled component called 'Slot'
@@ -114,9 +144,15 @@ const MainSlotMachine = () => {
   return (
     <Parent>
       <SubDiv>
-        <Slots></Slots>
-
-        <Spin>Spin!</Spin>
+        <Slots>
+          <Slot color={newColors[0]} borderRadius="10px 0 0 10px" />
+          <Slot color={newColors[1]} />
+          <Slot color={newColors[2]} borderRadius="0 10px 10px 0" />
+        </Slots>
+        {/* Disable button on 5 wins */}
+        <Spin onClick={spin} disabled={tally.wins >= 5}>
+          Spin!
+        </Spin>
       </SubDiv>
       <SubDiv>
         <Header>Tally</Header>
